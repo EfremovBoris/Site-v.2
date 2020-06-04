3a6adff25cced4da563a5d6c2f014032c9b4d68f
EfremovBoris <lovekamski@mail.ru>
2020-06-05 01:22:35 +0300

    add SCROLL to the TOP button

--- public/js/core.js
+++ public/js/core.js
@@ -24,16 +24,51 @@
         });
 
         //  GO to TOP button activation  /////////////////////////////////////////////////
-        
-        // function scrollFunction() {
-        //     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        //       document.getElementById("navbar").style.padding = "30px 10px";
-        //       document.getElementById("logo").style.fontSize = "25px";
-        //     } else {
-        //       document.getElementById("navbar").style.padding = "80px 10px";
-        //       document.getElementById("logo").style.fontSize = "35px";
-        //     }
-        //   }
+        $(document).scroll(function() {
+            var y = $(this).scrollTop();
+            if (y > 200) {
+              $('.scroll-top-btn').fadeIn();
+            } else {
+              $('.scroll-top-btn').fadeOut();
+            }
+          });
+          
+              // Select all links with hashes
+          $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function(event) {
+              // On-page links
+              if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+                && 
+                location.hostname == this.hostname
+              ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                  // Only prevent default if animation is actually gonna happen
+                  event.preventDefault();
+                  $('html, body').animate({
+                    scrollTop: target.offset().top
+                  }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                      return false;
+                    } else {
+                      $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                      $target.focus(); // Set focus again
+                    };
+                  });
+                }
+              }
+            });
 
 
         // InstagramFeed init /////////////////////////////////////////////////
