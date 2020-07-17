7f0d7f6732fee3b298724997f03d500502ba8fad
EfremovBoris <lovekamski@mail.ru>
2020-07-17 15:03:03 +0300

    Add medium photos, some ajustments

--- public/test.php
+++ public/test.php
@@ -1,6 +1,11 @@
-<?php $IPATH = $_SERVER["DOCUMENT_ROOT"]; 
+<?
+
+// php $IPATH = $_SERVER["DOCUMENT_ROOT"]; 
+// echo $IPATH;
+
+$IPATH = $_SERVER["DOCUMENT_ROOT"]."/components/"."menu.html";
 
 echo $IPATH;
 
-echo basename($_SERVER['SCRIPT_FILENAME']);
+// echo basename($_SERVER['SCRIPT_FILENAME']);
 ?>
